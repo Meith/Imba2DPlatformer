ddf386c8f73b1ca65f158d09afc41d3cdef204b4
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-19 14:39:18 -0400

    basic setup done of the fololwing: character movement, input controller, camera. Tweak according to game later.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+    public GameObject playerGameObject;
+
+    public float cameraXOffset;
+
+	// Update is called once per frame
+	void LateUpdate () 
+    {
+        transform.position = new Vector3(playerGameObject.transform.position.x - cameraXOffset, 0.0f, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float playerSpeed;
+
+    private Rigidbody playerRigidBody;
+
+	void Start () 
+    {
+        playerRigidBody = GetComponent<Rigidbody>();	
+	}
+	
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Add jump here later.
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        playerRigidBody.AddForce(movement * playerSpeed);
+    }
+}
