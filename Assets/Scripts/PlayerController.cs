20ec008fc43be277fbeb5d9b9538d61b7d035f83
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-19 20:59:48 -0400

    Improved PlayerController and added jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,46 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-    public float playerSpeed;
+    public float playerMovementSpeed;
+    public float playerJumpSpeed;
+
+    private bool playerIsGrounded;
+
+    private float moveHorizontal = 0.0f;
+    private float moveVertical = 0.0f;
 
     private Rigidbody playerRigidBody;
 
 	void Start () 
     {
+        playerIsGrounded = true;
         playerRigidBody = GetComponent<Rigidbody>();	
 	}
 	
-	void FixedUpdate()
+	void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        // Add jump here later.
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-
-        playerRigidBody.AddForce(movement * playerSpeed);
+        moveHorizontal = Input.GetAxis("Horizontal");
+        
+        if(Input.GetButtonDown("Jump") && playerIsGrounded)
+        {
+            moveVertical = 1.0f;
+            playerIsGrounded = false;
+        }
     }
+
+    void FixedUpdate()
+    {
+        Vector3 force = new Vector3(moveHorizontal * playerMovementSpeed, moveVertical * playerJumpSpeed, 0.0f);
+        playerRigidBody.AddForce(force);
+
+        if (!playerIsGrounded)
+            moveVertical = 0.0f;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Platform")
+            playerIsGrounded = true;
+    }
+
 }
